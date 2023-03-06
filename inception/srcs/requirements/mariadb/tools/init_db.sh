6d505af0ca02d9ffa4f57c5e2122f982b89a02b6
KimJiyoon <jjuni28@naver.com>
2023-03-06 14:54:56 +0900

    [Feat] 연결

--- inception/srcs/requirements/mariadb/tools/init_db.sh
+++ inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -1,19 +1,19 @@
 mkdir -p /auth_pam_tool_dir
 touch /auth_pam_tool_dir/auth_pam_tool
 
-mysql_install_db --user=root \
+mysql_install_db --user=mysql \
 								--basedir=/usr \
 								--datadir=/var/lib/mysql \
 								--skip-test-db
 
 cat > /tmp/init_db_sql << EOF
 FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'jiyokim'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'jiyokim'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
-/usr/bin/mysqld -u root --bootstrap < /tmp/init_db_sql
-/usr/bin/mysqld -u root
+/usr/bin/mysqld -u mysql --bootstrap < /tmp/init_db_sql
+/usr/bin/mysqld -u mysql
--- inception/srcs/requirements/wordpress/tools/wp.sh
+++ inception/srcs/requirements/wordpress/tools/wp.sh
@@ -1,2 +1,15 @@
-wp core download
+
+db_name=`wp config get DB_NAME 2> /dev/null`
+wp_admin=`wp user get admin --field=user_login 2> /dev/null`
+wp_user=`wp user get ${WP_USER} --field=user_login 2> /dev/null`
+
+if [ ! -f "/var/www/html/index.php" ]; then
+  wp core download
+	wp config create --dbhost=mariadb:3306 --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_USER_PASSWORD}
+  wp core install --url=$WP_URL --title=$WP_TITLE \
+                --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS \
+                --admin_email=$WP_ADMIN_MAIL --skip-email
+  wp user create $WP_USER_NAME $WP_USER_MAIL --user_pass=$WP_USER_PASS
+fi
+
 php-fpm81 --nodaemonize
