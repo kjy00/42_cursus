da8791362127b0475a6c9f39efefb39201f92d58
KimJiyoon <jjuni28@naver.com>
2023-03-09 02:16:09 +0900

    [Feat] mariadb dockerfile 설치 패키지 수정 \n크롬 설치\n이미지 이름 수정

--- inception/init_linux.sh
+++ inception/init_linux.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo apt-get install make
+sudo apt-get install make vim
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 sudo apt-get install \
     ca-certificates \
     curl \
